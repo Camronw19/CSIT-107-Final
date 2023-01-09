3ed03492557f3a7a35191831fdf7c7be7ad1c3aa
Camron Walsh <camronwalsh@gmail.com>
2023-01-08 20:44:59 -0500

    Navbar and home page

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Navbar, Home, About} from "./components"
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <About />
+    </div>
+  );
+}
+
+export default App
