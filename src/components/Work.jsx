0797426751bc439b624f7c36f21d3ca819fd7cd2
Camron Walsh <camronwalsh@gmail.com>
2023-01-12 17:38:48 -0500

    Worked on work section

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -21,100 +21,22 @@ const Work = () => {
             </div>
 
             {/* Container */}
-            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4 '>
                 {/* grid item */}
-                <Card className={`h-80 `}>
-                    <CardHeader color="gray" className="relative h-56 cursor-pointer transform transition-all hover:-translate-y-4">
-                    <img
-                        src={Sampler_JUCE}
-                        alt="img-blur-shadow"
-                        className="h-full w-full "
-                    />
-                    </CardHeader>
-                    <CardBody className="text-center">
-                    <Typography variant="h5" className="mb-2 font-poppins">
-                        JUCE Sampler Plug-in
-                    </Typography>
-                    <Typography className="font-poppins" >
-                        Audio VST created using C++ and the JUCE framework 
-                    </Typography>
-                    </CardBody>
-                    <CardFooter divider className="flex items-center justify-between py-3">
-                    <Typography className="font-poppins" variant="small">C++, JUCE</Typography>
-                    <Typography variant="small" color="gray" className="flex gap-1 font-poppins">
-                        <i className="fas fa-map-marker-alt fa-sm mt-[3px]" />
-                        Personal project
-                    </Typography>
-                    </CardFooter> 
-                </Card>
-
-                {/* grid item */}
-                <Card className={`h-80 `}>
-                    <CardHeader color="gray" className="relative h-56 cursor-pointer transform transition-all hover:-translate-y-4">
-                    <img
-                        src={Sampler_JUCE}
-                        alt="img-blur-shadow"
-                        className="h-full w-full "
-                    />
-                    </CardHeader>
-                    <CardBody className="text-center">
-                    <Typography variant="h5" className="mb-2">
-                        JUCE Sampler Plug-in
-                    </Typography>
-                    <Typography>
-                        Audio Sampler VST created using C++ and the JUCE framework 
-                    </Typography>
-                    </CardBody>
-                    <CardFooter divider className="flex items-center justify-between py-3">
-                    <Typography variant="small">C++, JUCE</Typography>
-                    <Typography variant="small" color="gray" className="flex gap-1">
-                        <i className="fas fa-map-marker-alt fa-sm mt-[3px]" />
-                        Personal project
-                    </Typography>
-                    </CardFooter> 
-                </Card>
-
-                {/* grid item */}
-                <Card className={`h-80 content-div2 `}>
-                    <CardHeader color="gray" className="relative h-56 cursor-pointer transform transition-all hover:-translate-y-4">
-                    <img
-                        src={Sampler_JUCE}
-                        alt="img-blur-shadow"
-                        className="h-full w-full "
-                    />
-                    </CardHeader>
-                    <CardBody className="text-center">
-                    <Typography variant="h5" className="mb-2">
-                        JUCE Sampler Plug-in
-                    </Typography>
-                    <Typography>
-                        Audio Sampler VST created using C++ and the JUCE framework 
-                    </Typography>
-                    </CardBody>
-                    <CardFooter divider className="flex items-center justify-between py-3">
-                    <Typography variant="small">C++, JUCE</Typography>
-                    <Typography variant="small" color="gray" className="flex gap-1">
-                        <i className="fas fa-map-marker-alt fa-sm mt-[3px]" />
-                        Personal project
-                    </Typography>
-                    </CardFooter> 
-                </Card>
-                
-                {/* grid item */}
-                <div style={{backgroundImage: `url(${realEstate})`}}
-                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div'>
+                <div style={{backgroundImage: `url(${Sampler_JUCE})`}}
+                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div duration-500'>
                   
                     {/* Hover Effects */}
-                    <div className='opacity-0 group-hover:opacity-100'>
+                    <div className='opacity-0 group-hover:opacity-100 duration-300'>
                         <span className='text-2xl font-bold text-white tracking-wider'>
-                        React JS Application
+                        Sampler VST
                         </span>
                         <div className='pt-8 text-center'>
                             <a href="/">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                                <button className='text-center rounded-lg px-4 py-3 m-2  text-white border-2  items-center  hover:border-[#7aa1c5] font-bold text-lg hover:bg-[#7aa1c5] hover:scale-105 duration-300'>Demo</button>
                             </a>
                             <a href="/">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                                <button className='text-center rounded-lg px-4 py-3 m-2  text-white border-2  items-center  hover:border-[#7aa1c5] font-bold text-lg hover:bg-[#7aa1c5] hover:scale-105 duration-300'>Code</button>
                             </a>
                         </div>
                     </div>
@@ -125,16 +47,16 @@ const Work = () => {
                 className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div'>
                   
                     {/* Hover Effects */}
-                    <div className='opacity-0 group-hover:opacity-100'>
+                    <div className='opacity-0 group-hover:opacity-100 duration-300'>
                         <span className='text-2xl font-bold text-white tracking-wider'>
                         React JS Application
                         </span>
                         <div className='pt-8 text-center'>
                             <a href="/">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                                <button className='text-center rounded-lg px-4 py-3 m-2  text-white border-2  items-center  hover:border-[#7aa1c5] font-bold text-lg hover:bg-[#7aa1c5] hover:scale-105 duration-300'>Demo</button>
                             </a>
                             <a href="/">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                                <button className='text-center rounded-lg px-4 py-3 m-2  text-white border-2  items-center  hover:border-[#7aa1c5] font-bold text-lg hover:bg-[#7aa1c5] hover:scale-105 duration-300'>Code</button>
                             </a>
                         </div>
                     </div>
@@ -145,21 +67,20 @@ const Work = () => {
                 className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div'>
                   
                     {/* Hover Effects */}
-                    <div className='opacity-0 group-hover:opacity-100'>
+                    <div className='opacity-0 group-hover:opacity-100 duration-300'>
                         <span className='text-2xl font-bold text-white tracking-wider'>
                         React JS Application
                         </span>
                         <div className='pt-8 text-center'>
                             <a href="/">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                                <button className='text-center rounded-lg px-4 py-3 m-2  text-white border-2  items-center  hover:border-[#7aa1c5] font-bold text-lg hover:bg-[#7aa1c5] hover:scale-105 duration-300'>Demo</button>
                             </a>
                             <a href="/">
-                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                                <button className='text-center rounded-lg px-4 py-3 m-2  text-white border-2  items-center  hover:border-[#7aa1c5] font-bold text-lg hover:bg-[#7aa1c5] hover:scale-105 duration-300'>Code</button>
                             </a>
                         </div>
                     </div>
                 </div>
-
             </div>
         </div>
     </div>
